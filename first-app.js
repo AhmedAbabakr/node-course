005ba1cc7a18c944788ae05d255894eea79a793c
AhmedAbabakr <a.abobakr@intrazero.net>
2023-01-25 18:54:27 +0200

    express

--- first-app.js
+++ first-app.js
@@ -2,8 +2,17 @@
 // fs.writeFileSync("hello.txt","Hello World!");
 
 const http = require("http");
+const express = require("express");
+const app = express();
 
-const routes = require("./routes");
+app.use((request,response,next) => {
+    console.log("Middleware");
+    next();
+});
+app.use((request,response,next) => {
+    console.log("Another Middleware");
+});
+// const routes = require("./routes");
 function rqListener(message,response)
 {
     console.log(message);
@@ -11,6 +20,7 @@ function rqListener(message,response)
     // process.exit();
 }
 
-const server = http.createServer(routes);
+// const server = http.createServer(routes);
+const server = http.createServer(app);
 
 server.listen(3000);
