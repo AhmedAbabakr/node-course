43e6c92925f797b03851a1f411cb0d72c9f96384
AhmedAbabakr <a.abobakr@intrazero.net>
2023-02-01 11:40:07 +0200

    view

--- first-app.js
+++ first-app.js
@@ -3,15 +3,17 @@
 
 const http = require("http");
 const express = require("express");
+const path = require("path");
 const bodyParser = require('body-parser');
 const app = express();
 const routes =require('./routes');
 const { response } = require("express");
 
 app.use(express.urlencoded({extended:false}));
-app.use('/admin',routes); 
+app.use(routes); 
 app.use((request,response,next) => {
-    response.status(404).send("<h1>Page Not Found</h1>");
+    response.status(404).sendFile(path.join(__dirname,'views/pages','404.html'));
+  
 });
 
 // app.use(bodyParser.urlencoded({extended:false}));
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 router.get('/',(request,response,next) => {
-    response.send("<h1>Welcome Home</h1>");
-
+    // response.send("<h1>Welcome Home</h1>");
+    response.sendFile(path.join(__dirname,'../','views','welcome.html'));
 });
 module.exports = router;
