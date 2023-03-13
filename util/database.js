b79ce53e7cd29995349bd3ffdd2bb9b82989c3cb
AhmedAbabakr <ahmedababakr@yahoo.com>
2023-03-13 03:52:11 +0200

    mysql2

---
+++ util/database.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql2');
+const pool =mysql.createPool({
+    host:"localhost",
+    user:"root",
+    database:"test",
+    password:"123456789"
+});
+module.exports = pool.promise();
